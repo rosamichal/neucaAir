201c902d801908b1db58f987312c50c2d69cf960
Michał Rosa <michal.rosa@action.pl>
2024-09-27 00:34:18 +0200

    Added FlightId

---
+++ NeucaAir.Domain/ValueObjects/FlightId.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NeucaAir.Domain.ValueObjects
+{
+    public record FlightId
+    {
+        public string Id { get; }
+
+        public FlightId(string id)
+        {
+            if (!IsValid(id))
+            {
+                throw new ArgumentException("Invalid Flight Id format.");
+            }
+            Id = id;
+        }
+
+        private bool IsValid(string id)
+        {
+            return Regex.IsMatch(id, @"^[A-Z]{3}\d{5}[A-Z]{3}$");
+        }
+    }
+}
